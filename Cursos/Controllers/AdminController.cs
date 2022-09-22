285c2360aa28ab9e3d75a5709327294b4ddda1ba
CITAI9 <CITAI9@DESKTOP-RDBRQG8>
2022-09-22 13:44:06 -0500

    Ajuste en metodos para cursos y usuarios

--- Cursos/Controllers/AdminController.cs
+++ Cursos/Controllers/AdminController.cs
@@ -45,9 +45,9 @@ namespace Cursos.Controllers
         [HttpGet]
         public ActionResult Tablas()
         {
-            List<cursos> curso = new List<cursos>();
-            //string sql = "select cursos.id, cursos.nombre, modalidad.modalidad, cursos.lugar, cursos.horas, cursos.costo, cursos.costoPref, cursos.urlTemario, cursos.requisitos, cursos.criterioEval, cursos.imgUrl from cursos inner join modalidad on modalidad.id = cursos.modalidad;";
-            string sql = "select * from cursos;";
+            List<cursos> obj = new List<cursos>();
+            string sql = "select cursos.id, cursos.nombre, cursos.modalidad, modalidad.id, modalidad.modalidad, cursos.lugar, cursos.horas, cursos.costo, cursos.costoPref, cursos.urlTemario, cursos.requisitos, cursos.criterioEval, cursos.imgUrl from cursos inner join modalidad on modalidad.id = cursos.modalidad;";
+            //string sql = "select * from cursos;";
 
             using (SqlConnection cn = new SqlConnection(cadenaConexion))
             {
@@ -59,31 +59,35 @@ namespace Cursos.Controllers
                         while (dr.Read())
                         {
                             cursos c = new cursos();
+                            modalidad m = new modalidad();
                             c.id = dr.GetInt32(0);
                             c.nombre = dr.GetString(1);
                             c.modalidad = dr.GetInt32(2);
-                            //c.modalidad1.modalidad1 = dr.GetString(2);
-                            c.lugar = dr.GetString(3);
-                            c.horas = dr.GetInt32(4);
-                            c.costo = dr.GetDecimal(5);
-                            c.costoPref = dr.GetDecimal(6);
-                            c.urlTemario = dr.GetString(7);
-                            c.requisitos = dr.GetString(8);
-                            c.criterioEval = dr.GetString(9);
-                            curso.Add(c);
+                            m.id = dr.GetInt32(3);
+                            m.modalidad1 = dr.GetString(4);
+                            c.modalidad1 = m;
+                            c.lugar = dr.GetString(5);
+                            c.horas = dr.GetInt32(6);
+                            c.costo = dr.GetDecimal(7);
+                            c.costoPref = dr.GetDecimal(8);
+                            c.urlTemario = dr.GetString(9);
+                            c.requisitos = dr.GetString(10);
+                            c.criterioEval = dr.GetString(11);
+                            obj.Add(c);
+                             
                         }
                     }
                 }
             }
-            return View(curso);
+            return View(obj);
         }
 
         [HttpGet]
         public ActionResult TablasUsuarios()
         {
             List<usuarios> usuario = new List<usuarios>();
-            //string sql = "select cursos.id, cursos.nombre, modalidad.modalidad as modalidad, cursos.lugar, cursos.horas, cursos.costo, cursos.costoPref, cursos.urlTemario, cursos.requisitos, cursos.criterioEval, cursos.imgUrl from cursos inner join modalidad on modalidad.id = cursos.modalidad;";
-            string sql = "select * from usuarios;";
+            string sql = "select usuarios.id, rol.id, rol.rolUser, usuarios.rol, usuarios.nombre, usuarios.apellido, usuarios.email from usuarios inner join rol on rol.id = usuarios.rol;";
+            //string sql = "select * from usuarios;";
 
             using (SqlConnection cn = new SqlConnection(cadenaConexion))
             {
@@ -95,11 +99,15 @@ namespace Cursos.Controllers
                         while (dr.Read())
                         {
                             usuarios u = new usuarios();
+                            rol r = new rol();
                             u.id = dr.GetInt32(0);
-                            u.rol = dr.GetInt32(1);
-                            u.nombre = dr.GetString(2);
-                            u.apellido = dr.GetString(3);
-                            u.email = dr.GetString(4);
+                            r.id = dr.GetInt32(1);
+                            r.rolUser = dr.GetString(2);
+                            u.rol1 = r;
+                            u.rol = dr.GetInt32(3);
+                            u.nombre = dr.GetString(4);
+                            u.apellido = dr.GetString(5);
+                            u.email = dr.GetString(6);
                             usuario.Add(u);
                         }
                     }

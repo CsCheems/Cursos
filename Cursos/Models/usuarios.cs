cc746be905a241a42e3ca7a1700a9ae0f53fbbd4
JR <JR@DESKTOP-ADDCRJO>
2022-09-27 18:03:41 -0500

    agrego metodos a usuarios modelo

--- Cursos/Models/usuarios.cs
+++ Cursos/Models/usuarios.cs
@@ -4,8 +4,12 @@ namespace Cursos.Models
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data;
     using System.Data.Entity.Spatial;
+    using System.Data.SqlClient;
     using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
 
     public partial class usuarios
     {
@@ -32,7 +36,172 @@ namespace Cursos.Models
 
 
         //Metodos
-        
+        //static string cadenaConexion = "Data Source=DESKTOP-RDBRQG8;Initial Catalog=edcouteq;Integrated Security=true;";
+        static string cadenaConexion = "Data Source=DESKTOP-ADDCRJO;Initial Catalog=edcouteq;Integrated Security=true; user id=sa; pwd=123";
 
-    }
+        //Metodo para obtener datos del usuario
+        public List<usuarios> GetUsuarios()
+        {
+            List<usuarios> usuarios = new List<usuarios>();
+            List<rol> rol = new List<rol>();
+
+            string sql = "select usuarios.id, usuarios.nombre, usuarios.apellido, rol.id, rol.rolUser, usuarios.email, usuarios.pass from usuarios inner join rol on rol.id = usuarios.rol;";
+            using(SqlConnection cn = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand(sql, cn);
+                cn.Open();
+
+                using(SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        usuarios u = new usuarios();
+                        rol r = new rol();
+                        u.id = dr.GetInt32(0);
+                        u.nombre = dr.GetString(1);
+                        u.apellido = dr.GetString(2);
+                        r.id = dr.GetInt32(3);
+                        r.rolUser = dr.GetString(4);
+                        u.rol1 = r;
+                        u.email = dr.GetString(5);
+                        u.pass = dr.GetString(6);
+                        usuarios.Add(u);
+                    }
+                }
+            }
+            return usuarios;
+        }
+
+        //Metodo para agregar usuarios
+        public bool InsertaUsuario(usuarios usuarioInfo)
+        {
+            bool registrado;
+            string mensaje;
+            if (usuarioInfo.pass == usuarioInfo.passConfirm)
+            {
+                usuarioInfo.pass = EncriptarSha256(usuarioInfo.pass);
+
+            }
+            else
+            {
+                return false;
+            }
+
+            using (SqlConnection cn = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("SP_registraUsuario", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("Rol", usuarioInfo.rol);
+                cmd.Parameters.AddWithValue("Nombre", usuarioInfo.nombre);
+                cmd.Parameters.AddWithValue("Apellido", usuarioInfo.apellido);
+                cmd.Parameters.AddWithValue("Email", usuarioInfo.email);
+                cmd.Parameters.AddWithValue("Pass", usuarioInfo.pass);
+                cmd.Parameters.Add("Registrado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+                
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                registrado = Convert.ToBoolean(cmd.Parameters["Registrado"].Value);
+                mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                cn.Close();
+            }
+
+            if (registrado)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Metodo para encriptar password
+        public static string EncriptarSha256(String texto)
+        {
+            StringBuilder sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(texto));
+
+                foreach (byte b in result)
+                    sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        //Metodo para editar usuario
+        public bool EditarUsuario(usuarios usuarioInfo)
+        {
+            bool registrado;
+            string mensaje;
+            if (usuarioInfo.pass == usuarioInfo.passConfirm)
+            {
+                usuarioInfo.pass = EncriptarSha256(usuarioInfo.pass);
+
+            }
+            else
+            {
+                return false;
+            }
+
+            using (SqlConnection cn = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("SP_editaUsuario", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("Id", usuarioInfo.id);
+                cmd.Parameters.AddWithValue("Rol", usuarioInfo.rol);
+                cmd.Parameters.AddWithValue("Nombre", usuarioInfo.nombre);
+                cmd.Parameters.AddWithValue("Apellido", usuarioInfo.apellido);
+                cmd.Parameters.AddWithValue("Email", usuarioInfo.email);
+                cmd.Parameters.AddWithValue("Pass", usuarioInfo.pass);
+                cmd.Parameters.Add("Registrado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+              
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                registrado = Convert.ToBoolean(cmd.Parameters["Registrado"].Value);
+                mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                cn.Close();
+            }
+
+            if (registrado)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        //Metodo para eliminar usuario
+        public bool eliminarUsuario(int id)
+        {
+            int i;
+            using (SqlConnection cn = new SqlConnection(cadenaConexion))
+            {
+                SqlCommand cmd = new SqlCommand("SP_eliminaUsuario", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("Id", id);
+
+
+                cn.Open();
+                i = cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }   
 }

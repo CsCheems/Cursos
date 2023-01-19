a88673c02cf5638d1c86e9dbfb1af917deb47469
CITAI9 <CITAI9@DESKTOP-RDBRQG8>
2023-01-19 02:04:18 -0600

    Se creo clase conexion a BD

---
+++ Cursos/BDConnection/SQL_DB_Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Cursos.BDConnection
+{
+    public class SQL_DB_Connection
+    {
+        public static string cadenaConexion = "Data Source=DESKTOP-RDBRQG8;Initial Catalog=edcouteq; user id=adminedco; pwd=edco_uteq_2022**";
+    }
+}

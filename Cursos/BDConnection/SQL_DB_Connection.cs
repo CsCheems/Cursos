ba18538b26e0155efa04108acb4c7c83ec6118e1
Chems <Chems@DESKTOP-8PFJ47P>
2023-06-15 09:48:25 -0600

    coneccion SQL

--- Cursos/BDConnection/SQL_DB_Connection.cs
+++ Cursos/BDConnection/SQL_DB_Connection.cs
@@ -9,8 +9,8 @@ namespace Cursos.BDConnection
 {
     public class SQL_DB_Connection
     {
-        public static string cadenaConexion = "Data Source=DESKTOP-DSCTT3S;Initial Catalog=edcouteq1; user id=; pwd=";
-        //public static string cadenaConexion = "Data Source=DESKTOP-8PFJ47P;Initial Catalog=edcouteq1; user id=sa; pwd=9Gag94lol";
+        //public static string cadenaConexion = "Data Source=DESKTOP-DSCTT3S;Initial Catalog=edcouteq1; user id=; pwd=";
+        public static string cadenaConexion = "Data Source=DESKTOP-8PFJ47P;Initial Catalog=edcouteq1; user id=sa; pwd=9Gag94lol";
         //public static string cadenaConexion = "Data Source=DESKTOP-M3IAU23;Initial Catalog=edcouteq1; user id=sa; pwd=9Gag94lol";
     }
 }
